b4e9da084669081debcac4e09665c4ac472df044
Elias Santos <79068489+Dolias18@users.noreply.github.com>
2022-01-28 10:27:34 -0300

    Commit 1

---
+++ js/main.js
@@ -0,0 +1,35 @@
+var dev = document.querySelector('.dev');
+var unip = document.querySelector('.unip');
+var video = document.querySelector('.video');
+var fundacao = document.querySelector('.fundacao');
+
+function curso001() {
+  if(dev.style.display === 'block') {
+      dev.style.display = 'none';
+  } else {
+      dev.style.display = 'block';
+  }
+};
+
+function curso002() {
+    if(unip.style.display === 'block') {
+        unip.style.display = 'none';
+    } else {
+        unip.style.display = 'block';
+    }
+};
+
+function curso003() {
+    if(video.style.display === 'block') {
+        video.style.display = 'none';
+    } else {
+        video.style.display = 'block';
+    }
+};
+function curso004() {
+    if(fundacao.style.display === 'block') {
+        fundacao.style.display = 'none';
+    } else {
+        fundacao.style.display = 'block';
+    }
+};
